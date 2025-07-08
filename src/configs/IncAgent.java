a2748fffa0c2a53c1c6ba8468097b3b5246c79c7
glhila <hilaglazz13@gmail.com>
2025-07-09 01:47:33 +0300

    Add IncAgent class implementation and remove old test version. Introduced a new agent for incrementing values with improved structure and functionality, including subscription management and message handling.

--- src/configs/IncAgent.java
+++ src/configs/IncAgent.java
@@ -1,7 +1,11 @@
-package test;
+/* 
+package configs;
 
 import java.util.ArrayList;
 
+import test.Agent;
+import test.Message;
+import test.TopicManagerSingleton;
 import test.TopicManagerSingleton.TopicManager;
 
 public class IncAgent implements Agent {
@@ -55,4 +59,47 @@ public class IncAgent implements Agent {
     }
 
     
+}
+*/
+package configs;
+
+import test.Agent;
+import test.Message;
+import test.TopicManagerSingleton;
+import test.TopicManagerSingleton.TopicManager;
+
+
+public class IncAgent implements Agent {
+    private double value = 0;
+    private final String[] subs;
+    private final String[] pubs;
+
+    public IncAgent(String[] subs, String[] pubs) {
+        this.subs = subs;
+        this.pubs = pubs;
+        TopicManagerSingleton.get().getTopic(subs[0]).subscribe(this);
+    }
+
+    @Override
+    public String getName() {
+        return "IncAgent";
+    }
+
+    @Override
+    public void reset() {
+        value = 0;
+    }
+
+    @Override
+    public void callback(String topic, Message msg) {
+        value = msg.asDouble;
+        if (!Double.isNaN(value)) {
+            TopicManagerSingleton.get().getTopic(pubs[0]).publish(new Message(value + 1));
+        }
+    }
+
+    @Override
+    public void close() {
+        TopicManagerSingleton.get().getTopic(subs[0]).unsubscribe(this);
+    }
 }
